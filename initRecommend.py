7eda213ff0c85c8349e39af0f1e72026fd3cdf84
at-phucpham <phuc.pham@asiantech.vn>
2019-06-13 23:59:37 +0700

    add

--- initRecommend.py
+++ initRecommend.py
@@ -21,7 +21,7 @@ def recommendBook(data_from_database, averageEvaluation):
     data_neighbors = pd.DataFrame(index=data_item_base_frame.columns, columns = range(1, 11))
 #     # Order by similarity
     for i in range(0, len(data_item_base_frame.columns)):
-        print( data_item_base_frame.iloc[0:, i].sort_values(ascending=True)[1:11].index)
+#         print( data_item_base_frame.iloc[0:, i].sort_values(ascending=True)[1:11].index)
         data_neighbors.iloc[i,:10] = data_item_base_frame.iloc[0:, i].sort_values(ascending=True)[1:11].index 
         
     data_sims = pd.DataFrame(index=data_neighbors.index,columns=range(1, 11))
@@ -63,5 +63,36 @@ def recommendBookForUser(data_from_database,id_user,recommend_data, averageEvalu
     return;
     
 def reRecommendBookForUser(data_from_database,recommend_data, averageValue):
-    recommend_data = recommendBook(data_from_database, averageValue);
-    return
+    data_item_base_frame = pd.DataFrame(index=data_from_database.index, columns=data_from_database.index)
+    # print data_item_base_frame.head(6).ix[:,0:6]
+    for i in data_item_base_frame.columns:
+        u = data_from_database.loc[i,:];
+        u1 = u.index.tolist();
+        d1 = [u[index] if u[index] > 0 else averageValue[index] for index in u1];
+        for j in data_item_base_frame.columns:
+#             print("Dong ",i," Cot ", j)
+            v = data_from_database.loc[j,:];
+            v1 = v.index.tolist();
+            d2 = [v[index] if v[index] > 0 else averageValue[index] for index in v1];
+            data_item_base_frame.loc[i, j] = distance.euclidean(d1,d2)
+#     data_item_base_frame.to_csv('data_user_base_frame.csv', sep=',')
+    # data_item_base_frame = pd.read_csv('data_item_base_frame.csv')
+    # print(data_item_base_frame.head(6).iloc[:,0:5])
+    # Initial a frame for save closes neighbors to an item
+    data_neighbors = pd.DataFrame(index=data_item_base_frame.columns, columns = range(1, 11))
+#     # Order by similarity
+    for i in range(0, len(data_item_base_frame.columns)):
+#         print( data_item_base_frame.iloc[0:, i].sort_values(ascending=True)[1:11].index)
+        data_neighbors.iloc[i,:10] = data_item_base_frame.iloc[0:, i].sort_values(ascending=True)[1:11].index 
+        
+    for i in range(0, len(data_item_base_frame.index)):
+        list2 = data_from_database.iloc[i,:].tolist();
+        data_tmp = pd.DataFrame(index=data_neighbors.iloc[i,:], columns = data_from_database.columns)
+        for k in data_tmp.index:
+            list1 = data_from_database.loc[k,:];
+            list_tmp = list1.index.tolist();
+            listCompare = [list1[ind] if list1[ind] > 0 else averageValue[ind] for ind in list_tmp]
+            data_tmp.loc[k,:] = numpy.subtract(listCompare,list2);
+            
+        recommend_data.iloc[i,:] = data_tmp.max().nlargest(10).index
+    return recommend_data
--- main.py
+++ main.py
@@ -10,15 +10,17 @@ import _thread
 
 passcode = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJMb2dpbiIsImRhdGEiOnsiaWQiOjEyLCJ1c2VybmFtZSI6ImNhb2RhbzEyMzQzNCJ9LCJpYXQiOjE1NDQxODk5MjMsImV4cCI6MTU0NTA1MzkyM30.P4_BTd-ZuOtvQd0QlazlKigmDl9sVnvoKgjWjXmspdA";
 allUser = getListData("select id from users where id != 0 and role_id = 3");
-allItem = getListData("select bid from book");
+allItem = getListData("select bid from book where is_active = true");
 data_from_database = pd.DataFrame(index=allUser,columns=allItem)
 data_from_database[:] = 0;
 
-allBying = getListObject("SELECT id_user, id_pro FROM orders INNER JOIN detailorder ON orders.id_order = detailorder.id_order WHERE id_user != 0 GROUP BY id_user, id_pro")  
+allBying = getListObject("SELECT id_user, id_pro FROM orders INNER JOIN detailorder ON orders.id_order = detailorder.id_order WHERE id_user != 0 AND orders.id_status != 2 GROUP BY id_user, id_pro")  
 for i in allBying:
     if(i[0] in data_from_database.index and i[1] in data_from_database.columns):
         data_from_database.loc[i[0],i[1]] = 1;
-
+        
+# print(allBying)        
+# print(data_from_database.loc[:27,43:])
 dictAverageScore = {};
 
 for i in data_from_database.columns:
@@ -29,6 +31,7 @@ for i in data_from_database.columns:
         dictAverageScore[i] = numpy.sum(listData) / len(data_from_database.index);
      
 recommend_data = recommendBook(data_from_database, dictAverageScore) 
+# print(data_from_database.loc[:27,43:])
 print(recommend_data)
 app = Flask(__name__)
 api = Api(app)
@@ -55,6 +58,7 @@ class AddNewBook(Resource):
         global dictAverageScore
         dictAverageScore[id_book] = 0;
         global data_from_database
+        global recommend_data
         data_from_database.insert(loc=0, column=id_book, value= [0] * len(data_from_database.index))
 #         data_from_database = data_from_database.assign(id_location=[-5] * len(data_from_database.index))
 #         data_from_database.rename(columns={'id_location': id_location}, inplace=True)
@@ -70,7 +74,9 @@ class AddNewUser(Resource):
         id_user = request.form['id_user']
         id_user = numpy.int64(id_user)
         
+        global dictAverageScore
         global data_from_database
+        global recommend_data
 #         columns_data = data_from_database.columns;
 #         data_add = [-5] * len(columns_data);
         data_from_database = data_from_database.append(pd.Series([0] * len(data_from_database.columns), index=data_from_database.columns, name=id_user))
@@ -78,9 +84,8 @@ class AddNewUser(Resource):
 #         data_from_database = data_from_database.append(pd.DataFrame(data_add,index=id_user,columns=columns_data))
 #         data_from_database.loc[id_user,:] = [-5] * len(data_from_database.columns);
 #         recommend_data.loc[id_user,:] = data_from_database.columns.sort_values(ascending=False)[0:10]
-        global recommend_data
         recommend_data = recommend_data.append(pd.Series(data_from_database.columns.sort_values(ascending=False)[0:10], index=recommend_data.columns, name=id_user))
-        _thread.start_new_thread(recommendBookForUser, (data_from_database, id_user, recommend_data, dictAverageScore) )
+        recommendBookForUser(data_from_database, id_user, recommend_data, dictAverageScore)
         print("AddNewUser ok")
         return {"data" : "OK"}
 
@@ -116,6 +121,71 @@ class BuyBook(Resource):
         print("BUY BOOK ok")
         return {"data" : "OK"}
 
+class CancelBuyBook(Resource):
+    def post(self):
+        passcodeFromClient = request.headers.get('passcode')
+        if passcodeFromClient != passcode:
+            return
+        list_id_book = [int(x) for x in request.form['list_id_book'].split()] 
+        
+        id_user = request.form['id_user']
+        id_user = numpy.int64(id_user)
+        id_order = request.form['id_order']
+        id_order = numpy.int64(id_order)
+        
+        global dictAverageScore
+        global data_from_database
+        global recommend_data
+        
+        is_change = False
+        for id_book in list_id_book:
+            if id_book in data_from_database.columns:
+                strSQl = 'select id_detailOrder from detailorder INNER JOIN orders ON detailorder.id_order = orders.id_order where id_status != 2 and id_pro = ' + str(id_book) + ' and detailorder.id_order != ' + str(id_order) + ' and id_user = ' + str(id_user);
+                list_id_detailOrder = getListData(strSQl);
+                if not(list_id_detailOrder) or len(list_id_detailOrder) == 0 :
+                    data_from_database.loc[id_user,id_book] = 0; 
+                    listData = [item for item in data_from_database.loc[:,id_book].tolist() if item >= 0];
+                    if len(listData) == 0:
+                        dictAverageScore[id_book] = 0;
+                    else:
+                        dictAverageScore[id_book] = numpy.sum(listData) / len(data_from_database.index);
+                    is_change = True
+                        
+        
+        if is_change == True:     
+            _thread.start_new_thread(recommendBookForUser, (data_from_database, id_user, recommend_data, dictAverageScore) )        
+        print("CANCEL BUY BOOK ok")
+        return {"data" : "OK"}
+
+class ActiveBook(Resource):
+    def post(self):
+        passcodeFromClient = request.headers.get('passcode')
+        if passcodeFromClient != passcode:
+            return
+        
+        id_book = request.form['id_book']
+        id_book = numpy.int64(id_book)
+        
+        global dictAverageScore
+        global data_from_database
+        global recommend_data
+        
+        
+        if id_book not in data_from_database.columns:
+            data_from_database.insert(loc=0, column=id_book, value= [0] * len(data_from_database.index))
+            strSQl = 'select id_user from detailorder INNER JOIN orders ON detailorder.id_order = orders.id_order where id_status != 2 and id_pro = ' + str(id_book);
+            list_id_user = getListData(strSQl);
+            if list_id_user and len(list_id_user) > 0 :
+                dictAverageScore[id_book] = len(list_id_user) / len(data_from_database.index);
+                for id_user in list_id_user:
+                    data_from_database.loc[id_user,id_book] = 1;
+#                     _thread.start_new_thread(recommendBookForUser, (data_from_database, id_user, recommend_data, dictAverageScore) )
+            else:
+                dictAverageScore[id_book] = 0;       
+        _thread.start_new_thread(reRecommendBookForUser,(data_from_database,recommend_data, dictAverageScore ))                 
+        print("ACTIVE BOOK ok")
+        return {"data" : "OK"}
+
 class DeleteUser(Resource):
     def post(self):
         passcodeFromClient = request.headers.get('passcode')
@@ -123,6 +193,9 @@ class DeleteUser(Resource):
             return
         id_user = request.form['id_user']
         id_user = numpy.int64(id_user)
+        global dictAverageScore
+        global data_from_database
+        global recommend_data
         data_from_database.drop(id_user,inplace=True)
         recommend_data.drop(id_user,inplace=True)
         print("DELETE USER ok")
@@ -135,17 +208,26 @@ class DeleteBook(Resource):
             return
         id_book = request.form['id_book']
         id_book = numpy.int64(id_book)
+        
+        global dictAverageScore
+        global data_from_database
+        global recommend_data
+        
         del dictAverageScore[id_book]
         del data_from_database[id_book]
-        recommend_data.replace(id_book,-1,inplace=True)
-        _thread.start_new_thread(reRecommendBookForUser, (data_from_database, recommend_data, dictAverageScore ))
-        print("AddNewUser ok")
+        #recommend_data.replace(id_book,-1,inplace=True)
+        list_id_user = [id_user for id_user in data_from_database.index if id_book in recommend_data.loc[id_user, :].tolist() ]
+        for id_user in list_id_user:
+            _thread.start_new_thread(recommendBookForUser, (data_from_database, id_user, recommend_data, dictAverageScore) )
+        print("DeleteBook ok")
         return {"data" : "OK" } 
 #     
 api.add_resource(RecommendBook, '/recommendBook/<id_user>')
 api.add_resource(AddNewBook, '/addBook')
 api.add_resource(AddNewUser, '/addUser')
 api.add_resource(BuyBook, '/buyBook')
+api.add_resource(ActiveBook, '/activeBook')
+api.add_resource(CancelBuyBook, '/cancelBuyBook')
 api.add_resource(DeleteUser, '/deleteUser')
 api.add_resource(DeleteBook, '/deleteBook')
     
